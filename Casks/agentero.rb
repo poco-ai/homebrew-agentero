5d2e2067904091f23307af814b423ee76414bd34
Phil-Fan <hw.phil.fan@gmail.com>
2026-07-21 13:40:18 +0800

    feat(cask): support Linux AppImage builds

--- Casks/agentero.rb
+++ Casks/agentero.rb
@@ -5,13 +5,28 @@ cask "agentero" do
   # automatically by the release workflow in poco-ai/Agentero. Do not install
   # from this tap until the first release has updated them.
 
-  on_arm do
-    sha256 "105acdc8e706a3271ed2bf4f524fe85d99063ab08586032484608bd8283f8dad"
-    url "https://github.com/poco-ai/Agentero/releases/download/v#{version}/Agentero_#{version}_aarch64.dmg"
+  on_macos do
+    on_arm do
+      sha256 "105acdc8e706a3271ed2bf4f524fe85d99063ab08586032484608bd8283f8dad"
+      url "https://github.com/poco-ai/Agentero/releases/download/v#{version}/Agentero_#{version}_aarch64.dmg"
+    end
+    on_intel do
+      sha256 "PLACEHOLDER_SHA256_X64_DMG"
+      url "https://github.com/poco-ai/Agentero/releases/download/v#{version}/Agentero_#{version}_x64.dmg"
+    end
   end
-  on_intel do
-    sha256 "PLACEHOLDER_SHA256_X64_DMG"
-    url "https://github.com/poco-ai/Agentero/releases/download/v#{version}/Agentero_#{version}_x64.dmg"
+
+  on_linux do
+    on_arm do
+      sha256 "PLACEHOLDER_SHA256_AARCH64_LINUX_APPIMAGE"
+      url "https://github.com/poco-ai/Agentero/releases/download/v#{version}/Agentero_#{version}_aarch64.AppImage"
+      binary "Agentero_#{version}_aarch64.AppImage", target: "agentero"
+    end
+    on_intel do
+      sha256 "PLACEHOLDER_SHA256_AMD64_LINUX_APPIMAGE"
+      url "https://github.com/poco-ai/Agentero/releases/download/v#{version}/Agentero_#{version}_amd64.AppImage"
+      binary "Agentero_#{version}_amd64.AppImage", target: "agentero"
+    end
   end
 
   name "Agentero"
